10a92082ad9bdf8212496c7a8648d12ba3f43dce
RamkrishnaBaddi@gmail.com <you@example.com>
2018-11-25 13:46:12 +0800

    Initial Commit

---
+++ DateTimePicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DateTimePicker
+//
+//  Created by Ramkrishna Baddi on 25/11/18.
+//  Copyright © 2018 Ramkrishna Baddi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
